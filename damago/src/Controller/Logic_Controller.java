66e5c63251123a9a2439fce518d56bdd69d622cb
smhrd <smhrd@211.63.240.47>
2022-01-18 18:43:20 +0900

    수정

--- damago/src/Controller/Logic_Controller.java
+++ damago/src/Controller/Logic_Controller.java
@@ -6,8 +6,5 @@ public class Logic_Controller {
 		//select(); // 메뉴 선택 번호 입력받기
 		//process(); // 메뉴 작업에 따른 기능 수행 // 만일 JDBC 작업이라면 JDBC_Controller 객체에서 호출.
 	}
-	
-	public void menu() {
-		
-	}
+
 }
