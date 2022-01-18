53f2d09b2c703a2a3ed05ceb3736040ef97f3201
smhrd <smhrd@211.227.224.169>
2022-01-18 12:35:38 +0900

    테스트2

--- damago/src/View/View.java
+++ damago/src/View/View.java
@@ -3,8 +3,13 @@ package View;
 public class View {
 
 	public static void main(String[] args) {
-		System.out.println("테스트입니다");
 		System.out.println("test3");
+		System.out.println("test3");
+		System.out.println("test3");
+		System.out.println("test3");
+		System.out.println("test3");
+		System.out.println("test3");
+		
 	}
 
 }
