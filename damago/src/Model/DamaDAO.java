4422719126be83b201674a1126804bbd8a6e0281
smhrd <smhrd@59.0.236.242>
2022-01-18 16:11:01 +0900

    전체 사용자의 다마고치 출력

--- damago/src/Model/DamaDAO.java
+++ damago/src/Model/DamaDAO.java
@@ -108,9 +108,52 @@ public class DamaDAO {
 		return check;
 	}
 	
-	// 로그인 한 사용자의 다마고치를 ArrayList에 넣음
-	public ArrayList<DamaVO> selectDamaList(String id) {
-		ArrayList<DamaVO> P_list = new ArrayList<DamaVO>();
+	// 전체 사용자의 다마고치 출력(랭킹용)
+	public ArrayList<DamaVO> DamaRank() {
+		ArrayList<DamaVO> rank_list = new ArrayList<DamaVO>();
+
+		try {
+			connect();
+
+			// 3. 실행할 SQL문 정의
+			String sql = "select * from dama";
+
+			// 4. SQL구문 실행 준비 객체(PreparedStatement) 생성
+			// prepareStatement(정의할 sql);
+			pst = conn.prepareStatement(sql);
+
+			// 5. sql문을 실행하고 결과 처리
+			// executeQuery : select -> 검색(table상에 변화가 일어나지 않음)
+			// 반환타입 : ResultSet이라는 객체를 반환
+			rs = pst.executeQuery();
+
+			// dama 테이블의 값을 읽어서 출력
+			while (rs.next()) {
+				int num = rs.getInt(1); // 커서가 가리키고 있는 행의 첫번째 column값을 읽어옴
+				String nick = rs.getString("nick");
+				String type = rs.getString("type");
+				int exp = rs.getInt("exp");
+				int energy = rs.getInt("energy");
+				String id = rs.getString("id");
+				String date = rs.getString("date");
+
+				// 위에서 읽어온 값들로 초기화시켜 생성한 DamaVO 객체의 참조값을
+				// ArrayList에 추가
+				rank_list.add(new DamaVO(type, nick, num, exp, energy, id, date));
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			// 객체들 마무리(Connection, PreparedStatement, ResultSet)
+			close();
+		}
+		return rank_list;
+	}
+	
+	// 로그인 한 사용자의 다마고치 출력
+	public ArrayList<DamaVO> selectDama(String id) {
+		ArrayList<DamaVO> dama_list = new ArrayList<DamaVO>();
 
 		try {
 			connect();
@@ -141,7 +184,7 @@ public class DamaDAO {
 
 				// 위에서 읽어온 값들로 초기화시켜 생성한 DamaVO 객체의 참조값을
 				// ArrayList에 추가
-				P_list.add(new DamaVO(type, nick, num, exp, energy, id, date));
+				dama_list.add(new DamaVO(type, nick, num, exp, energy, id, date));
 			}
 
 		} catch (Exception e) {
@@ -150,6 +193,7 @@ public class DamaDAO {
 			// 객체들 마무리(Connection, PreparedStatement, ResultSet)
 			close();
 		}
-		return P_list;
+		return dama_list;
 	}
+	
 }
