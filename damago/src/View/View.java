18e52dccba8efb2c869d3ea429d0d40a49b79754
tkroahdla@naver.com <tkroahdla@naver.com>
2022-01-19 23:38:03 +0900

    사망 선택 불가 구현

--- damago/src/View/View.java
+++ damago/src/View/View.java
@@ -59,31 +59,36 @@ public class View {
 									System.out.println(sel_list.get(i).toString());
 								}
 								DamaDTO sel_dama = dama.select(sel_list);
-								
-								//인자로 리스트
-								System.out.println();
 
-								int needs = lc.DamaNeeds(); // needs에 욕망 인덱스 저장..
-
-								while (true) {// 키우기 그만할때 까지
-									System.out.println("현재 경험치 : " + sel_dama.getExp());
-									System.out.println("청결도 : " + sel_dama.getCleaning());
-									System.out.println("현재 에너지" + sel_dama.getEnergy());
-									System.out.println("현재 EXP" + sel_dama.getExp());
-									System.out.println("◆ 1.밥먹이기\t◆ 2.잠재우기\t◆ 3.놀아주기 \n◆ 4.운동시키기\t◆ 5.씻겨주기\t◆ 6.뒤로가기");
-									lc.sel_need(sel_dama, needs);
-
+								if (sel_dama.getEnergy() == 999) {
+									System.out.println("이미 사망한 다마고치에요...");
+								} else {
+									// 인자로 리스트
 									System.out.println();
-									if (sel_dama.isStop()) {
-										break; // 뒤로가기.
-									}
-									lc.energyCheck(sel_dama);
-									lc.expCheck(sel_dama);
-									
-									dama.DamaUpdate(sel_dama); // column.. energy, exp는 2자리 number 그렇다면.. 에너지는 더이상
-									if(sel_dama.getEnergy()>100) { // 차지않도록하고 .. 경험치는 바로 레벨로 전환될수잇도록.
-										System.out.println(sel_dama.getNick() + "(이)가 죽었어요..");
-										break;
+
+									int needs = lc.DamaNeeds(); // needs에 욕망 인덱스 저장..
+
+									while (true) {// 키우기 그만할때 까지
+										System.out.println("현재 경험치 : " + sel_dama.getExp());
+										System.out.println("청결도 : " + sel_dama.getCleaning());
+										System.out.println("현재 에너지" + sel_dama.getEnergy());
+										System.out.println("현재 EXP" + sel_dama.getExp());
+										System.out.println(
+												"◆ 1.밥먹이기\t◆ 2.잠재우기\t◆ 3.놀아주기 \n◆ 4.운동시키기\t◆ 5.씻겨주기\t◆ 6.뒤로가기");
+										lc.sel_need(sel_dama, needs);
+
+										System.out.println();
+										if (sel_dama.isStop()) {
+											break; // 뒤로가기.
+										}
+										lc.energyCheck(sel_dama);
+										lc.expCheck(sel_dama);
+
+										dama.DamaUpdate(sel_dama); // column.. energy, exp는 2자리 number 그렇다면.. 에너지는 더이상
+										if (sel_dama.getEnergy() > 100) { // 차지않도록하고 .. 경험치는 바로 레벨로 전환될수잇도록.
+											System.out.println(sel_dama.getNick() + "(이)가 죽었어요..");
+											break;
+										}
 									}
 								}
 							}
@@ -93,7 +98,7 @@ public class View {
 							for (DamaDTO vo : rank_list) {
 								System.out.println(vo.toString());
 							}
-						} else if (dama_menu == 4) { //삭제
+						} else if (dama_menu == 4) { // 삭제
 							sel_list = dama.selectDama(login);// 리스트만 존재함 출력해야 확인가능
 							if (sel_list.size() == 0) {
 								System.out.println("삭제 할 다마고치가 없어요!! \n다마고치를 등록해주세요..");
